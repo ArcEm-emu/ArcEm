106353344802e889a9e45aa1cc5b362eb27118f6
Peter Howkins <flibble@users.sourceforge.net>
2005-01-30 15:59:27 +0000

    posix version of the filecalls abstraction layer.

---
+++ X/filecalls_internal.h
@@ -0,0 +1,17 @@
+/* filecalls_internal.h
+   Copyright (c) 2005 Peter Howkins, covered under the GNU GPL see file COPYING for more
+   details */
+
+#ifndef __FILECALLS_INTERNAL_H
+#define __FILECALLS_INTERNAL_H
+
+#include <sys/types.h>
+#include <dirent.h>
+
+struct Directory_s {
+  DIR *hDir;
+};
+
+#define ARCEM_PATH_MAX PATH_MAX
+
+#endif /* __FILECALLS_H */
