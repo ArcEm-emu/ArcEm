7fa46283d6e1566f8c955252f31c67658c47660e
Peter Howkins <flibble@users.sourceforge.net>
2006-02-05 23:39:45 +0000

    First bash at holding all the configuration values for a running copy of Arcem.
    Handles initialising the values on startup (to their current defaults)
    
    Adds an standard C commandline parser for platforms that support it. (To be tested and enabled properly).

---
+++ arch/ArcemConfig.c
@@ -0,0 +1,181 @@
+/*
+  ArcemConfig
+
+  (c) 2006 P Howkins
+
+  Part of Arcem released under the GNU GPL, see file COPYING
+  for details.
+
+  ArcemConfig - Stores the running configuration of the Arcem
+  emulator.
+ 
+  See usage instructions in the ArcemConfig.h header file.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ArcemConfig.h"
+#include "Version.h"
+
+
+// Global Variable holding the configuration runtime options of
+// the emulator
+ArcemConfig hArcemConfig;
+
+/** 
+ * ArcemConfig_SetupDefaults
+ *
+ * Called on program startup, sets up the defaults for
+ * all the configuration values for the emulator
+ */
+void ArcemConfig_SetupDefaults(void)
+{
+  // Default to 4MB of memory
+  hArcemConfig.eMemSize = MemSize_4M;
+
+  // We default to an ARM 2AS architecture (includes SWP) without a cache
+  hArcemConfig.eProcessor = Processor_ARM250;
+}
+
+/**
+ * ArcemConfig_ParseCommandLine
+ *
+ * Given the commandline arguments that the program was started with,
+ * parse and fill in the runtime options into the global configuration
+ * structure. Will exit the program if command line arguments are
+ * incorrectly formatted (or the --help or --version argument is used).
+ *
+ * @param argc Number of entries in argv
+ * @param argv Array of char*'s represented space seperated commandline arguments
+ */
+void ArcemConfig_ParseCommandLine(int argc, char *argv[])
+{
+  int iArgument = 0;
+  char sHelpString[] =
+    "Arcem <Options>\n" \
+    " Where options are one or more of the following\n" \
+    "  --help - Display this message and exit\n" \
+    "  --version - Display the version number and exit\n" \
+    "  --memory <value> - Set the memory size of the emulator\n" \
+    "     Where value is one of '256K', '512K', '1M', '2M', '4M',\n" \
+    "     '8M', '12M' or '16M'\n" \
+    "  --processor <value> - Set the emulated CPU\n" \
+    "     Where value is one of 'ARM2', 'ARM250', 'ARM3'\n";
+
+  // No commandline arguments?
+  if(0 == argc) {
+    // There should always be at least 1, the program name
+    return;
+  }
+
+  if(1 == argc) {
+    // No arguments other than the program name, no work to do
+    return;
+  }
+
+  // We don't care about argument 0 as that's the program name
+  iArgument = 1;
+
+  while(iArgument < argc) {
+    if(0 == strcmp("--version", argv[iArgument])) {
+      printf("Arcem %s\n", Version);
+      
+      exit(EXIT_SUCCESS);
+    }
+    else if(0 == strcmp("--version", argv[iArgument])) {
+      printf("%s", sHelpString);
+      
+      exit(EXIT_SUCCESS);
+    }
+    else if(0 == strcmp("--memory", argv[iArgument])) {
+      if(iArgument+1 < argc) { // Is there a following argument?
+        if(0 == strcmp("256K", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_256K;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("512K", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_512K;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("1M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_1M;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("2M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_2M;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("4M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_4M;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("8M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_8M;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("12M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_12M;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("16M", argv[iArgument + 1])) {
+          hArcemConfig.eMemSize = MemSize_16M;
+
+          iArgument += 2;
+        }
+        else {
+          fprintf(stderr, "Unrecognised value to the --memory option\n");
+
+          exit(EXIT_FAILURE);
+        }
+
+      } else {
+        // No argument following the --memory option
+        fprintf(stderr, "No argument following the --memory option\n");
+
+        exit(EXIT_FAILURE);
+      }
+    }
+    else if(0 == strcmp("--processor", argv[iArgument])) {
+      if(iArgument+1 < argc) { // Is there a following argument?
+        if(0 == strcmp("ARM2", argv[iArgument + 1])) {
+          hArcemConfig.eProcessor = Processor_ARM2;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("ARM250", argv[iArgument + 1])) {
+          hArcemConfig.eProcessor = Processor_ARM250;
+
+          iArgument += 2;
+        }
+        else if(0 == strcmp("ARM3", argv[iArgument + 1])) {
+          hArcemConfig.eProcessor = Processor_ARM3;
+
+          iArgument += 2;
+        }
+        else {
+          fprintf(stderr, "Unrecognised value to the --processor option\n");
+
+          exit(EXIT_FAILURE);
+        }
+      } else {
+        // No argument following the --processor option
+        fprintf(stderr, "No argument following the --processor option\n");
+
+        exit(EXIT_FAILURE);
+      }
+    }
+    else {
+      fprintf(stderr, "Unrecognised option '%s', try --help\n", argv[iArgument]);
+
+      exit(EXIT_FAILURE);
+    }
+  }
+}
---
+++ arch/ArcemConfig.h
@@ -0,0 +1,89 @@
+/*
+  ArcemConfig
+
+  (c) 2006 P Howkins
+
+  Part of Arcem released under the GNU GPL, see file COPYING
+  for details.
+
+  ArcemConfig - Stores the running configuration of the Arcem
+  emulator.
+ 
+  FUTURE VERSIONS will absorb the code from ReadConfig.c
+
+  The Structure hArcemConfig can be filled in by means other
+  than the Command Line parser as needed on a platform by
+  platform basis.
+
+  Using a configuration value in the rest of the emulator code
+  ------------------------------------------------------------
+   Use the global variable 'hArcemConfig'. Its properties are defined
+  in the struct ArcemConfig_s below. Use enumeration values or #defines
+  for all properties that support that.
+
+  Adding a config value to the emulator
+  -------------------------------------
+   1) Add a value of the correct type to ArcemConfig.
+      If possible use an enumeration or #defines for
+      its values.
+   2) Make sure the value is initialised to its default
+      value in ArcemConfig_SetupDefaults()
+   3) Add a way of setting it to alternate values in
+      ArcemConfig_ParseCommandLine()
+   4) If ways of setting the config other than Command Line
+      are implimented add code for parsing and setting values
+      there too.
+*/
+
+typedef enum ArcemConfig_MemSize_e {
+  MemSize_256K,
+  MemSize_512K,
+  MemSize_1M,
+  MemSize_2M,
+  MemSize_4M,
+  MemSize_8M,
+  MemSize_12M,
+  MemSize_16M
+} ArcemConfig_MemSize;
+
+typedef enum ArcemConfig_Processor_e {
+  Processor_ARM2,                 // ARM 2 
+  Processor_ARM250,               // ARM 2AS
+  Processor_ARM3                  // ARM 2AS
+} ArcemConfig_Processor;
+
+
+/* THIS IS THE MAIN CONFIGURATION STRUCTURE */
+
+typedef struct ArcemConfig_s {
+  ArcemConfig_MemSize   eMemSize;
+  ArcemConfig_Processor eProcessor; 
+
+  // TODO
+  // MFM Disc details (make sure the ReadConfig stuff fills in these details)
+  // hostfs disc location
+  // extnrom directory
+} ArcemConfig;
+
+extern ArcemConfig hArcemConfig;
+
+/** 
+ * ArcemConfig_SetupDefaults
+ *
+ * Called on program startup, sets up the defaults for
+ * all the configuration values for the emulator
+ */
+extern void ArcemConfig_SetupDefaults(void);
+
+/**
+ * ArcemConfig_ParseCommandLine
+ *
+ * Given the commandline arguments that the program was started with,
+ * parse and fill in the runtime options into the global configuration
+ * structure. Will exit the program if command line arguments are
+ * incorrectly formatted (or the --help or --version argument is used).
+ *
+ * @param argc Number of entries in argv
+ * @param argv Array of char*'s represented space seperated commandline arguments
+ */
+extern void ArcemConfig_ParseCommandLine(int argc, char *argv[]);
