6814342f68a5374cfc4f3b5fa0e1d96cffeba193
Peter Howkins <flibble@users.sourceforge.net>
2006-02-19 16:26:41 +0000

    Indenting normalised to 2 spaces.

--- arch/keyboard.c
+++ arch/keyboard.c
@@ -11,14 +11,14 @@
 
 /* Information on an arch_key_id. */
 typedef struct {
-    const char *name;
-    unsigned char row;
-    unsigned char col;
+  const char *name;
+  unsigned char row;
+  unsigned char col;
 } key_info;
 
 static key_info keys[] = {
 #define X(key, row, col) { #key, row, col },
-    ARCH_KEYBOARD_DEFINITION
+  ARCH_KEYBOARD_DEFINITION
 #undef X
 };
 
@@ -36,28 +36,27 @@ static key_info keys[] = {
 /* ------------------------------------------------------------------ */
 
 void keyboard_key_changed(struct arch_keyboard *kb, arch_key_id kid,
-    int up)
+                          int up)
 {
-    key_info *ki;
-    KbdEntry *e;
+  key_info *ki;
+  KbdEntry *e;
 
-    ki = keys + kid;
+  ki = keys + kid;
 
-    DBG((stderr, "keyboard_key_changed(kb, \"%s\", %d)\n", ki->name,
-        up));
+  DBG((stderr, "keyboard_key_changed(kb, \"%s\", %d)\n", ki->name, up));
 
-    if (kb->BuffOcc >= KBDBUFFLEN) {
-        fprintf(stderr, "keyboard_key_changed: key \"%s\" discarded, "
+  if (kb->BuffOcc >= KBDBUFFLEN) {
+    fprintf(stderr, "keyboard_key_changed: key \"%s\" discarded, "
             "buffer full\n", ki->name);
-        return;
-    }
-
-    e = kb->Buffer + kb->BuffOcc++;
-    e->KeyColToSend = ki->col;
-    e->KeyRowToSend = ki->row;
-    e->KeyUpNDown = up;
-
     return;
+  }
+
+  e = kb->Buffer + kb->BuffOcc++;
+  e->KeyColToSend = ki->col;
+  e->KeyRowToSend = ki->row;
+  e->KeyUpNDown = up;
+
+  return;
 }
 
 /* ------------------------------------------------------------------ */
@@ -66,83 +65,81 @@ void keyboard_key_changed(struct arch_keyboard *kb, arch_key_id kid,
 
 void Kbd_StartToHost(ARMul_State *state)
 {
-    /* Start a particular transmission - based on the HostCommand
-     * value.  This is either a request for keyboard id, request for
-     * mouse, or 0.  If it's 0 we should check to see if a key needs
-     * sending or mouse data needs sending. */
+  /* Start a particular transmission - based on the HostCommand
+   * value.  This is either a request for keyboard id, request for
+   * mouse, or 0.  If it's 0 we should check to see if a key needs
+   * sending or mouse data needs sending. */
 
-    if (KBD.KbdState != KbdState_Idle) {
-        return;
-    }
-
-    if (KBD.HostCommand) {
-        DBG((stderr, "Kbd_StartToHost: HostCommand=%d\n",
-            KBD.HostCommand));
-    }
-
-    switch (KBD.HostCommand) {
-    case 0x20: /* Request keyboard id */
-        if (IOC_WriteKbdRx(state, PROTO_KBID_UK) != -1) {
-            KBD.KbdState = KbdState_Idle;
-            KBD.HostCommand = 0;
-        }
-        return;
-    case 0x22: /* Request mouse position */
-        KBD.MouseXToSend = KBD.MouseXCount;
-        KBD.MouseYToSend = KBD.MouseYCount;
-        if (IOC_WriteKbdRx(state, KBD.MouseXToSend) != -1) {
-            KBD.KbdState = KbdState_SentMouseByte1;
-            KBD.HostCommand = 0;
-        }
-        return;
-    }
-
-    /* OK - we'll have a look to see if we should be doing something
-     * else */
-    if (KBD.KbdState != KbdState_Idle) {
-        return;
-    }
-
-    /* Perhaps some keyboard data */
-    if (KBD.KeyScanEnable && KBD.BuffOcc > 0) {
-        int loop;
-
-        DBG((stderr, "KBD_StartToHost - sending key -  BuffOcc=%d "
-            "(%d,%d,%d)\n", KBD.BuffOcc, KBD.Buffer[0].KeyUpNDown,
-            KBD.Buffer[0].KeyRowToSend, KBD.Buffer[0].KeyColToSend));
-
-        KBD.KeyUpNDown = KBD.Buffer[0].KeyUpNDown;
-        KBD.KeyRowToSend = KBD.Buffer[0].KeyRowToSend;
-        KBD.KeyColToSend = KBD.Buffer[0].KeyColToSend;
-
-        /* I should implement a circular buffer - but can't be bothered
-         * yet. */
-        for(loop = 1; loop < KBD.BuffOcc; loop++) {
-            KBD.Buffer[loop - 1] = KBD.Buffer[loop];
-        }
-
-        if (IOC_WriteKbdRx(state, (KBD.KeyUpNDown ? 0xd0 : 0xc0) |
-            KBD.KeyRowToSend) != -1) {
-            KBD.KbdState = KbdState_SentKeyByte1;
-        }
-        KBD.BuffOcc--;
-        return;
-    }
-
-    /* NOTE:  Mouse movement gets lower priority than key input. */
-    if (KBD.MouseTransEnable && KBD.MouseXCount | KBD.MouseYCount) {
-        /* Send some mouse data why not! */
-        KBD.MouseXToSend = KBD.MouseXCount;
-        KBD.MouseYToSend = KBD.MouseYCount;
-        KBD.MouseXCount = 0;
-        KBD.MouseYCount = 0;
-        if (IOC_WriteKbdRx(state, KBD.MouseXToSend) != -1) {
-            KBD.KbdState = KbdState_SentMouseByte1;
-        }
-        return;
-    }
-
+  if (KBD.KbdState != KbdState_Idle) {
     return;
+  }
+
+  if (KBD.HostCommand) {
+    DBG((stderr, "Kbd_StartToHost: HostCommand=%d\n", KBD.HostCommand));
+  }
+
+  switch (KBD.HostCommand) {
+    case 0x20: /* Request keyboard id */
+      if (IOC_WriteKbdRx(state, PROTO_KBID_UK) != -1) {
+        KBD.KbdState = KbdState_Idle;
+        KBD.HostCommand = 0;
+      }
+      return;
+    case 0x22: /* Request mouse position */
+      KBD.MouseXToSend = KBD.MouseXCount;
+      KBD.MouseYToSend = KBD.MouseYCount;
+      if (IOC_WriteKbdRx(state, KBD.MouseXToSend) != -1) {
+        KBD.KbdState = KbdState_SentMouseByte1;
+        KBD.HostCommand = 0;
+      }
+      return;
+  }
+
+  /* OK - we'll have a look to see if we should be doing something
+   * else */
+  if (KBD.KbdState != KbdState_Idle) {
+    return;
+  }
+
+  /* Perhaps some keyboard data */
+  if (KBD.KeyScanEnable && KBD.BuffOcc > 0) {
+    int loop;
+
+    DBG((stderr, "KBD_StartToHost - sending key -  BuffOcc=%d "
+        "(%d,%d,%d)\n", KBD.BuffOcc, KBD.Buffer[0].KeyUpNDown,
+        KBD.Buffer[0].KeyRowToSend, KBD.Buffer[0].KeyColToSend));
+
+    KBD.KeyUpNDown = KBD.Buffer[0].KeyUpNDown;
+    KBD.KeyRowToSend = KBD.Buffer[0].KeyRowToSend;
+    KBD.KeyColToSend = KBD.Buffer[0].KeyColToSend;
+
+    /* I should implement a circular buffer - but can't be bothered
+     * yet. */
+    for(loop = 1; loop < KBD.BuffOcc; loop++) {
+      KBD.Buffer[loop - 1] = KBD.Buffer[loop];
+    }
+
+    if (IOC_WriteKbdRx(state, (KBD.KeyUpNDown ? 0xd0 : 0xc0) | KBD.KeyRowToSend) != -1) {
+      KBD.KbdState = KbdState_SentKeyByte1;
+    }
+    KBD.BuffOcc--;
+    return;
+  }
+
+  /* NOTE:  Mouse movement gets lower priority than key input. */
+  if (KBD.MouseTransEnable && KBD.MouseXCount | KBD.MouseYCount) {
+    /* Send some mouse data why not! */
+    KBD.MouseXToSend = KBD.MouseXCount;
+    KBD.MouseYToSend = KBD.MouseYCount;
+    KBD.MouseXCount = 0;
+    KBD.MouseYCount = 0;
+    if (IOC_WriteKbdRx(state, KBD.MouseXToSend) != -1) {
+      KBD.KbdState = KbdState_SentMouseByte1;
+    }
+    return;
+  }
+
+  return;
 }
 
 /* ------------------------------------------------------------------ */
@@ -152,112 +149,112 @@ void Kbd_StartToHost(ARMul_State *state)
 
 void Kbd_CodeFromHost(ARMul_State *state, unsigned char FromHost)
 {
-    DBG((stderr, "Kbd_CodeFromHost: FromHost=0x%x State=%d\n",
-        FromHost, KBD.KbdState));
+  DBG((stderr, "Kbd_CodeFromHost: FromHost=0x%x State=%d\n",
+      FromHost, KBD.KbdState));
 
-    switch (KBD.KbdState) {
+  switch (KBD.KbdState) {
     case KbdState_JustStarted:
-        /* Only valid code is Reset. */
-        if (FromHost == PROTO_HRST) {
-            if (IOC_WriteKbdRx(state, PROTO_HRST) != -1) {
-                KBD.KbdState = KbdState_SentHardReset;
-                DBG((stderr, "KBD: Received Reset and sent Reset\n"));
-            } else {
-                DBG((stderr, "KBD: Couldn't respond to Reset - Kart "
-                    "full\n"));
-            }
+      /* Only valid code is Reset. */
+      if (FromHost == PROTO_HRST) {
+        if (IOC_WriteKbdRx(state, PROTO_HRST) != -1) {
+          KBD.KbdState = KbdState_SentHardReset;
+          DBG((stderr, "KBD: Received Reset and sent Reset\n"));
         } else {
-            DBG((stderr, "KBD: JustStarted; Got bad code 0x%x\n",
-                FromHost));
+          DBG((stderr, "KBD: Couldn't respond to Reset - Kart "
+              "full\n"));
         }
-        break;
+      } else {
+        DBG((stderr, "KBD: JustStarted; Got bad code 0x%x\n",
+            FromHost));
+      }
+      break;
 
     case KbdState_SentHardReset:
-        /* Only valid code is ack1 */
-        if (FromHost == PROTO_RAK1) {
-            if (IOC_WriteKbdRx(state, PROTO_RAK1) != -1) {
-                KBD.KbdState = KbdState_SentAck1;
-                DBG((stderr, "KBD: Received Ack1 and sent Ack1\n"));
-            } else {
-                DBG((stderr, "KBD: Couldn't respond to Ack1 - Kart "
-                    "full\n"));
-            }
+      /* Only valid code is ack1 */
+      if (FromHost == PROTO_RAK1) {
+        if (IOC_WriteKbdRx(state, PROTO_RAK1) != -1) {
+          KBD.KbdState = KbdState_SentAck1;
+          DBG((stderr, "KBD: Received Ack1 and sent Ack1\n"));
         } else {
-            DBG((stderr, "KBD: SentAck1; Got bad code 0x%x - "
-                "sending reset\n", FromHost));
-            IOC_WriteKbdRx(state, PROTO_HRST);
-            KBD.KbdState = KbdState_SentHardReset;
-            /* Or should that be just started? */
+          DBG((stderr, "KBD: Couldn't respond to Ack1 - Kart "
+              "full\n"));
         }
-        break;
+      } else {
+        DBG((stderr, "KBD: SentAck1; Got bad code 0x%x - "
+            "sending reset\n", FromHost));
+        IOC_WriteKbdRx(state, PROTO_HRST);
+        KBD.KbdState = KbdState_SentHardReset;
+        /* Or should that be just started? */
+      }
+      break;
 
     case KbdState_SentAck1:
-        /* Only valid code is Reset Ak 2 */
-        if (FromHost == PROTO_RAK2) {
-            if (IOC_WriteKbdRx(state, PROTO_RAK2) != -1) {
-                KBD.KbdState = KbdState_SentAck2;
-                DBG((stderr, "KBD: Received and ack'd to Ack2\n"));
-            } else {
-                DBG((stderr, "KBD: Couldn't respond to Ack2 - Kart "
-                    "full\n"));
-            }
+      /* Only valid code is Reset Ak 2 */
+      if (FromHost == PROTO_RAK2) {
+        if (IOC_WriteKbdRx(state, PROTO_RAK2) != -1) {
+          KBD.KbdState = KbdState_SentAck2;
+          DBG((stderr, "KBD: Received and ack'd to Ack2\n"));
         } else {
-            DBG((stderr, "KBD: SentAck2; Got bad code 0x%x\n",
-                FromHost));
+          DBG((stderr, "KBD: Couldn't respond to Ack2 - Kart "
+              "full\n"));
         }
-        break;
+      } else {
+        DBG((stderr, "KBD: SentAck2; Got bad code 0x%x\n",
+            FromHost));
+      }
+      break;
 
     default:
-        if (FromHost == PROTO_HRST) {
-            if (IOC_WriteKbdRx(state, PROTO_HRST) != -1) {
-                KBD.KbdState = KbdState_SentHardReset;
-            DBG((stderr, "KBD: Received and ack'd to hardware "
-                "reset\n"));
+      if (FromHost == PROTO_HRST) {
+        if (IOC_WriteKbdRx(state, PROTO_HRST) != -1) {
+          KBD.KbdState = KbdState_SentHardReset;
+          DBG((stderr, "KBD: Received and ack'd to hardware "
+              "reset\n"));
         } else {
-            DBG((stderr, "KBD: Couldn't respond to hardware reset - "
-                "Kart full\n"));
+          DBG((stderr, "KBD: Couldn't respond to hardware reset - "
+              "Kart full\n"));
         }
         return;
-    }
+      }
 
-    switch (FromHost & 0xf0) {
-    case 0: /* May be LED switch */
-        if ((FromHost & 0x08) == 0x08) {
+      switch (FromHost & 0xf0) {
+        case 0: /* May be LED switch */
+          if ((FromHost & 0x08) == 0x08) {
             DBG((stderr, "KBD: Received bad code: 0x%x\n",
                 FromHost));
-            return;
-        }
-        DBG((stderr, "KBD: LED state now: 0x%x\n", FromHost & 0x7));
-        if (KBD.Leds != (FromHost & 0x7)) {
+             return;
+          }
+          DBG((stderr, "KBD: LED state now: 0x%x\n", FromHost & 0x7));
+          if (KBD.Leds != (FromHost & 0x7)) {
             KBD.Leds = FromHost & 0x7;
             if (KBD.leds_changed) {
-                (*KBD.leds_changed)(KBD.Leds);
+              (*KBD.leds_changed)(KBD.Leds);
             }
-        }
+          }
 #ifdef LEDENABLE
-        /* I think we have to acknowledge - but I don't know with
-         * what. */
-        if (IOC_WriteKbdRx(state, 0xa0 | FromHost & 0x7)) {
+          /* I think we have to acknowledge - but I don't know with
+           * what. */
+          if (IOC_WriteKbdRx(state, 0xa0 | FromHost & 0x7)) {
             DBG((stderr, "KBD: acked led's\n"));
-        } else {
+          } else {
             DBG((stderr, "KBD: Couldn't respond to LED - Kart "
-                "full\n"));
-        };
+                 "full\n"));
+          }
 #endif
-        break;
+          break;
 
-    case 0x20: /* Host requests keyboard id - or mouse position. */
-        KBD.HostCommand = FromHost;
-        Kbd_StartToHost(state);
-        DBG((stderr, "KBD: Host requested keyboard id\n"));
-        return;
+        case 0x20: /* Host requests keyboard id - or mouse position. */
+          KBD.HostCommand = FromHost;
+          Kbd_StartToHost(state);
+          DBG((stderr, "KBD: Host requested keyboard id\n"));
+          return;
 
-    case 0x30: /* Its probably an ack of some type. */
-        switch (FromHost & 0xf) {
-        case 0: /* Disables everything. */
-        case 1: /* Disables mouse. */
-        case 2: /* Disables keys. */
-        case 3: /* all enabled. */
+        case 0x30: /* Its probably an ack of some type. */
+          switch (FromHost & 0xf) {
+          case 0: /* Disables everything. */
+          case 1: /* Disables mouse. */
+          case 2: /* Disables keys. */
+          case 3: /* all enabled. */
             if (KBD.KbdState != KbdState_SentKeyByte1 &&
                 KBD.KbdState != KbdState_SentMouseByte1) {
                 KBD.KbdState = KbdState_Idle;
@@ -272,50 +269,52 @@ void Kbd_CodeFromHost(ARMul_State *state, unsigned char FromHost)
             break;
 
         case 0xf: /* First byte ack. */
-            if (KBD.KbdState != KbdState_SentKeyByte1 &&
-                KBD.KbdState != KbdState_SentMouseByte1) {
-                DBG((stderr, "KBD: Got 1st byte ack when we "
-                    "haven't sent one!\n"));
+          if (KBD.KbdState != KbdState_SentKeyByte1 &&
+            KBD.KbdState != KbdState_SentMouseByte1)
+          {
+            DBG((stderr, "KBD: Got 1st byte ack when we "
+                 "haven't sent one!\n"));
+          } else {
+            if (KBD.KbdState == KbdState_SentMouseByte1) {
+              if (IOC_WriteKbdRx(state, KBD.MouseYToSend) == -1) {
+                  DBG((stderr, "KBD: Couldn't send 2nd byte "
+                      "of mouse value - Kart full\n"));
+              }
+              KBD.KbdState = KbdState_SentMouseByte2;
             } else {
-                if (KBD.KbdState == KbdState_SentMouseByte1) {
-                    if (IOC_WriteKbdRx(state, KBD.MouseYToSend) == -1) {
-                        DBG((stderr, "KBD: Couldn't send 2nd byte "
-                            "of mouse value - Kart full\n"));
-                    }
-                    KBD.KbdState = KbdState_SentMouseByte2;
-                } else {
-                    if (IOC_WriteKbdRx(state,
-                        (KBD.KeyUpNDown ? 0xd0 : 0xc0) |
-                        KBD.KeyColToSend) == -1) {
-                        DBG((stderr, "KBD: Couldn't send 2nd byte "
-                            "of key value - Kart full\n"));
-                    }
-                    KBD.KbdState = KbdState_SentKeyByte2;
-                    /* Indicate that the key has been sent. */
-                    KBD.KeyRowToSend = -1;
-                    KBD.KeyColToSend = -1;
-                }
-            } /* Have sent 1st byte test. */
-            break;
+              if (IOC_WriteKbdRx(state,
+                  (KBD.KeyUpNDown ? 0xd0 : 0xc0) |
+                  KBD.KeyColToSend) == -1)
+              {
+                  DBG((stderr, "KBD: Couldn't send 2nd byte "
+                      "of key value - Kart full\n"));
+              }
+              KBD.KbdState = KbdState_SentKeyByte2;
+              /* Indicate that the key has been sent. */
+              KBD.KeyRowToSend = -1;
+              KBD.KeyColToSend = -1;
+            }
+          } /* Have sent 1st byte test. */
+          break;
 
         default:
-            DBG((stderr, "KBD: Bad ack type received 0x%x\n",
-                FromHost));
-            break;
-        }
+          DBG((stderr, "KBD: Bad ack type received 0x%x\n",
+              FromHost));
+          break;
+      }
+      return;
+
+      case 0x40: /* Host just sends us some data. */
+        DBG((stderr, "KBD: Host sent us some data: 0x%x\n",
+            FromHost));
         return;
 
-        case 0x40: /* Host just sends us some data. */
-            DBG((stderr, "KBD: Host sent us some data: 0x%x\n",
-                FromHost));
-            return;
-
-        default:
-            DBG((stderr, "KBD: Unknown code received from host "
-                "0x%x\n", FromHost));
-            return;
-        }
+      default:
+        DBG((stderr, "KBD: Unknown code received from host "
+            "0x%x\n", FromHost));
+        return;
     }
+  }
 
-    return;
+  return;
 }
