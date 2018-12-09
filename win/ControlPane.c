eb84727f3879d3a14d71988b3f70a96964e8e557
Cameron Cawley <ccawley2011@gmail.com>
2018-12-09 20:43:50 +0000

    Use a MessageBox to display errors on Windows

--- win/ControlPane.c
+++ win/ControlPane.c
@@ -10,6 +10,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <windows.h>
+
 void ControlPane_Init(ARMul_State *state)
 {
 
@@ -17,10 +19,13 @@ void ControlPane_Init(ARMul_State *state)
 
 void ControlPane_Error(int code,const char *fmt,...)
 {
+  char err[100];
   va_list args;
   va_start(args,fmt);
   /* Log it */
   vfprintf(stderr,fmt,args);
+  vsnprintf(err, sizeof(err), fmt, args);
+  MessageBoxA(NULL, err, "ArcEm", MB_ICONERROR);
   /* Quit */
   exit(code);
 }
