9e4b23f4237b59f33ac072987b3143f351c98c90
Cameron Cawley <ccawley2011@gmail.com>
2024-11-10 16:45:32 +0000

    Improve c99.h with older versions of Visual Studio

--- c99.h
+++ c99.h
@@ -15,7 +15,7 @@
 
 /* For now, assume we're using a C99 compiler */
 
-#if defined(_MSC_VER) && (_MSC_VER < 1800)
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
 typedef __int8 int8_t;
 typedef __int16 int16_t;
 typedef __int32 int32_t;
@@ -30,19 +30,31 @@ typedef int32_t int_fast16_t;
 typedef uint8_t uint_fast8_t;
 typedef uint32_t uint_fast16_t;
 
+typedef int8_t int_least8_t;
+typedef int16_t int_least16_t;
+typedef uint8_t uint_least8_t;
+typedef uint16_t uint_least16_t;
+
 #define UINT32_C(x) (x ## U)
 #define UINT64_C(x) (x ## ULL)
+
+#define INT32_MIN (-2147483647i32 - 1)
+#define INT32_MAX 2147483647i32
+#define UINT32_MAX 0xffffffffui32
 #else
 #include <stdint.h>
 #endif
 
-#ifdef _MSC_VER
-#define inline __inline
+#if defined(_MSC_VER) && (_MSC_VER < 1800)
 typedef unsigned char bool;
 #define true 1
 #define false 0
 #else
 #include <stdbool.h>
+#endif
+
+#ifdef _MSC_VER
+#define inline __inline
 #endif
 
 #endif
